f0a6882c7cb014122fbc9046ccb3433e6a30efae
rubychew <brianoneill.mail@gmail.com>
2024-12-05 22:00:51 +0000

    create basic ui

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/")
 def get_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/register")
+def register(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
+@app.get("/audio-files")
+def list_files(request: Request):
+    return templates.TemplateResponse("audio-files.html", {"request": request})
