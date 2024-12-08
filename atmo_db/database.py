8b17af356c419887b091966890ddabde3e8a4d67
rubychew <brianoneill.mail@gmail.com>
2024-12-08 21:50:08 +0000

    tidy file

--- atmo_db/database.py
+++ atmo_db/database.py
@@ -2,8 +2,6 @@ from sqlmodel import Session, create_engine
 import os
 
 #Database setup
-DB_PATH = os.path.join(os.path.dirname(__file__), 'db', 'atmo.db')
-print(f'db path: {DB_PATH}')
 DB_URL = "sqlite:////app/atmo_db/atmo.db"
 engine = create_engine(DB_URL, connect_args=
                       {'check_same_thread': False})
