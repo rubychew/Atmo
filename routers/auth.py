d7472167e0b6e00215f57a2ae359830d5afe8d3e
rubychew <brianoneill.mail@gmail.com>
2024-12-11 16:15:22 +0000

    login authentication added

--- routers/auth.py
+++ routers/auth.py
@@ -7,15 +7,24 @@ from atmo_db.models import User
 from datetime import datetime, timezone
 import re
 from atmo_db.database import get_session
-from sqlmodel import Session
+from sqlmodel import Session, select
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
+# helper method -----------------------------------------------------------------------------
+def test_email_domain(email: str):
+    # temporary domain restriction for application rollout
+    reg = "[A-Za-z]*.*\\d*@[a-z]*.*ncirl.ie"
+    compiled = re.compile(reg)
+    domain_valid = re.search(compiled, email)
 
-
+    if not domain_valid:
+        raise HTTPException(status_code=400, detail="Bad request.")
+    
+# routes -------------------------------------------------------------------------------------
 @router.get("/")
 async def get_sign_in(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
@@ -33,15 +42,10 @@ async def create_standard_user(username: Annotated[str, Form()],
                                repassword: Annotated[str, Form()], session: Session = Depends(get_session)):
     
     # temporary domain restriction for application rollout
-    reg = "[A-Za-z]*.*\\d*@[a-z]*.*ncirl.ie"
-    compiled = re.compile(reg)
-    domain_valid = re.search(compiled, email)
-
-    if not domain_valid:
-        raise HTTPException(status_code=400, detail="Bad request: Invalid Email Address")
+    test_email_domain(email)
     
     if password != repassword:
-        raise HTTPException(status_code=400, detail="Bad request: passowords do not match!")
+        raise HTTPException(status_code=400, detail="Bad request: passwords do not match!")
     
     #check for password complexty and password length min chars max 20
     reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!#%*?&_]{8,20}$"
@@ -72,3 +76,27 @@ async def create_standard_user(username: Annotated[str, Form()],
 @router.get("/registration-success")
 def register(request: Request):
     return templates.TemplateResponse("registration-success.html", {"request": request})
+
+
+# login / authentication routes -------------------------------------------------------------------
+
+@router.post("/login")
+async def authenticate_login(email: Annotated[str, Form()],
+                                password: Annotated[str, Form()],
+                                session: Session = Depends(get_session)):
+
+    # temporary domain restriction for application rollout
+    test_email_domain(email)
+    
+    statement = select(User).where(User.email == email)
+    user = session.exec(statement).first()
+
+    if not user:
+        return 'Authentication Failed'
+    
+    if not bcrypt_context.verify(password, user.password):
+        return 'Authentication Failed'
+    
+    # otherwise issue JWT token and redirect to TOTP page
+    return {'Success': 'logged in'}
+    
